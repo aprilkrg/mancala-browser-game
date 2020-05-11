498702bbe99e757f05c79c739c20c8f341a744a4
April <macuser@Mac-User.local>
2020-05-11 10:24:14 -0600

    commit 051120

--- js/main.js
+++ js/main.js
@@ -1,119 +1,106 @@
 // /*----- constants -----*/
 let baskets = [
-    {
-    name: 'a',
-    value: null,
-    },
-    {
-    name: 'b',
-    value: null,
-    },
-    {
-    name: 'c',
-    value: null,
-    },
-    {
-    name: 'd',
-    value: null,
-    },
-    {
-    name: 'e',
-    value: null,
-    },
-    {
-    name: 'f',
-    value: null,
-    },
-    {
-    name: 'p1Points',
-    value: null,
-    },
-    {
-    name: 'g',
-    value: null,
-    },
-    {
-    name: 'h',
-    value: null,
-    },
-    {
-    name: 'i',
-    value: null,
-    },
-    {
-    name: 'j',
-    value: null,
-    },
-    {
-    name: 'k',
-    value: null,
-    },
-    {
-    name: 'l',
-    value: null,
-    },
-    {
-    name: 'p2Points',
-    value: null,
-    }
+    {name: 'a',
+    value: null,},
+    { name: 'b',
+    value: null,},
+    { name: 'c',
+    value: null,},
+    { name: 'd',
+    value: null,},
+    { name: 'e',
+    value: null,},
+    { name: 'f',
+    value: null,},
+    { name: 'p1Points',
+    value: null,},
+    { name: 'g',
+    value: null,},
+    { name: 'h',
+    value: null,},
+    { name: 'i',
+    value: null,},
+    { name: 'j',
+    value: null,},
+    { name: 'k',
+    value: null,},
+    { name: 'l',
+    value: null,},
+    { name: 'p2Points',
+    value: null, },
   ];
 //  console.log(baskets[1].name) ////expected output 'b'
 players = [
-    {
-        name: 'p1',
+    {   name: 'p1',
         turn: '1',
         path: [ baskets[0], baskets[1], baskets[2], baskets[3], baskets[4], baskets[5], baskets[6], baskets[7], baskets[8], baskets[9], baskets[10], baskets[11], baskets[12] ],
         mySide: [ baskets[0], baskets[1], baskets[2], baskets[3], baskets[4], baskets[5] ],
     },
-    {
-        name: 'p2',
+    {   name: 'p2',
         turn: '-1',
         path: [ baskets[7], baskets[8], baskets[9], baskets[10], baskets[11], baskets[12], baskets[13], baskets[14], baskets[1], baskets[2], baskets[3], baskets[4], baskets[5], baskets[6] ],
         mySide: [ baskets[7], baskets[8], baskets[9], baskets[10], baskets[11], baskets[12] ],
-    }
+    },
 ];
 //console.log(players[0].path[2]) ////expected output 'name: 'c' '
 
 // /*----- app's state (variables) -----*/
-let playerTurn;
-//player array [mancala] length [point total]
-//array [basket] selected
-//array length [basket totals] after element [stone] is added
-//48 elements [stones]
-//let stoneEls;
+let winner;
+let turn;
 
 // /*----- cached DOM references -----*/
-//2 cached arrays [mancalas] for totalling points 
-
-//1 cached array [hand] representing the array [basket] selected by the player 
-//const hand = Array.from(document)
+const startBtnEl = document.querySelector('go');
+//console.log('btn', startBtnEl); //output null
+const mancalas = Array.from(document.querySelectorAll('mancala'));
+//console.log(mancalas); //output is switched, need to figure out how to flip
+const basketEls = document.querySelectorAll('board');
+//console.log(basketEls); //output is a nodelist
 
 // /*----- event listeners -----*/
-document.getElementById('go')
-    .addEventListener('click', init); //// [start new game]
-document.getElementById('board')
-    .addEventListener('click', playerTurn); //// [start new turn]
+// document.querySelector('go')
+// .addEventListener('click', init); //// [start new game]
 
+// document.getElementById('myside')
+//     .addEventListener('click', turn); //// [start new turn]
 
 // /*----- functions -----*/
-//starting the game:
 function init() {
+    baskets.value = 0;
+   let turn = 1;
+};
+init();
+// console.log('before render', baskets[0].value); //outcome null
+// console.log('before render', baskets[6].value); //outcome null
+
+function render() {
+    winner = getWinner();
+    renderBaskets();
+};
+// console.log('after render', baskets[0].value); //output null
+// console.log('after render', baskets[6].value); //output null
+
+function renderBaskets() {
     baskets.forEach(function(baskets) {
         baskets.value--;
         baskets.value = baskets.value < 1 ? 4 : baskets.value;
-        //baskets.value = baskets.getElementsById(!"mancala").value < 1 ? 4 : baskets.value;
-        //how do i exclude class id "mancala" from this?
-    });
-   // baskets.value = baskets.getElementById('mancala').value > 0 ? null : baskets.value;
-    console.log(baskets[1].value); //expected output 4
-    console.log(baskets[6].value); //expected output null
-    let playerTurn = 1;
+    }) 
+
 };
-init();
+renderBaskets();
+console.log('after renderbaskets', baskets[0].value); //output 4 
+console.log('after renderbaskets', baskets[6].value); //output 4 //DESIRED OUTPUT null
+          
+function workTheClick(evt) {
+    //get the 
+};
 
-
-// function playerTurn() {
+// function turn() {
+//         for (i = 0, i < 12, i++);
     
+//     };
+
+// function getWinner() {
+
 // };
     //render 4 elements [stones] in each array [basket] 
     //player turn:
@@ -139,3 +126,41 @@ init();
     //if player1 total > player2 total
     //player1 wins
     //else player2 wins
+
+//------------------pseudocode that's been moved around----------//
+    //2 cached arrays [mancalas] for totalling points 
+// const movingBasket = Array.from(document.getElementById('board'));
+//1 cached array [hand] representing the array [basket] selected by the player 
+// things access multiple times : mancalas, baskets, hand(function?), each players array for their turn
+
+//player array [mancala] length [point total]
+//array [basket] selected
+//array length [basket totals] after element [stone] is added
+//48 elements [stones]
+//let stoneEls;
+
+
+
+
+//////-------------taken from function render baskets ---------------//
+       // basketEls.forEach(function(basketEl)) {
+    //     basketEls[value].innerHTML(display);
+    // }
+   
+    // for (let basket in baskets) {
+        //     mancalas[basket].value = null;
+        // }  
+        
+        
+        //how do i exclude class id "mancala" from this?
+        //baskets.value = baskets.getElementsById(!"mancala").value < 1 ? 4 : baskets.value;
+    // baskets.querySelectorAll('mancala').value(value, null);
+    // baskets.querySelectorAll('mancala').value = 0;
+    // baskets.forEach(function(baskets) {
+        //     if (baskets.getElementsById('mancala')) {
+            //         baskets.value = baskets.value > 0 ? 0 : baskets.value;
+            //     } 
+            //     else if (baskets.getElementsById('board')) {
+                //         baskets.value = baskets.value > 0 ? 4 : baskets.value;
+                //     } return;
+                // });
